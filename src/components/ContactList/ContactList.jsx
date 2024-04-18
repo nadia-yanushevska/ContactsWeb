0ca7ef6a2ec102f480a5434810565a8a3a12a84d
Nadia <nadiazlagoda@gmail.com>
2024-04-18 11:33:30 +0300

    loading-remove

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,18 @@
 import Contact from '../Contact/Contact';
 import Notification from '../Notification/Notification';
 import s from './ContactList.module.css';
-import { selectError, selectLoading } from '../../redux/contacts/selectors';
+import { selectError } from '../../redux/contacts/selectors';
 import { useSelector } from 'react-redux';
-import Loader from '../Loader/Loader';
 import { selectFilteredContacts } from '../../redux/selectors';
 
 function ContactList() {
     const filteredContacts = useSelector(selectFilteredContacts);
-    const loading = useSelector(selectLoading);
     const error = useSelector(selectError);
 
     return (
         <>
-            {loading && <Loader />}
             {error && <Notification>Oops, something went wrong!</Notification>}
-            {!loading &&
-                !error &&
+            {!error &&
                 (filteredContacts.length > 0 ? (
                     <ul className={s.list}>
                         {[...filteredContacts].reverse().map(contact => {
