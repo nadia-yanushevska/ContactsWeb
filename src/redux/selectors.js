9b0f9f8829df30140f97c0b0d1d914b653206062
Nadia <nadiazlagoda@gmail.com>
2024-04-18 20:42:20 +0300

    filterNumber

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { selectContacts } from './contacts/selectors';
 import { selectNameFilter } from './filters/selectors';
+import { deformatNumber } from '../helpers/helpers';
 
 export const selectFilteredContacts = createSelector([selectContacts, selectNameFilter], (contacts, filterName) => {
-    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(filterName.toLocaleLowerCase()));
+    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(filterName.toLocaleLowerCase()) || deformatNumber(contact.number).includes(filterName));
 });
