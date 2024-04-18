b073786770565b782a077b2f59a131daa10fce68
Nadia <nadiazlagoda@gmail.com>
2024-04-18 21:06:24 +0300

    clearSearch

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,15 +3,28 @@ import { changeFilter } from '../../redux/filters/slice';
 
 import s from './SearchBox.module.css';
 import { selectNameFilter } from '../../redux/filters/selectors';
+import { IoMdClose } from 'react-icons/io';
+import { useEffect } from 'react';
 
 function SearchBox() {
     const dispatch = useDispatch();
     const filterValue = useSelector(selectNameFilter);
 
+    useEffect(() => {
+        return () => dispatch(changeFilter(''));
+    }, []);
+
     return (
         <label className={s.label}>
             Find contacts by name
-            <input type="text" className={s.input} value={filterValue} onChange={e => dispatch(changeFilter(e.target.value))} />
+            <div className={s.div}>
+                <input type="text" className={s.input} value={filterValue} onChange={e => dispatch(changeFilter(e.target.value))} />
+                {filterValue && (
+                    <button type="button" className={s.btn} onClick={() => dispatch(changeFilter(''))}>
+                        <IoMdClose color="darkslategrey" size={28} />
+                    </button>
+                )}
+            </div>
         </label>
     );
 }
